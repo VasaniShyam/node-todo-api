5b70608bb71a4c3abf8087c4b05223f5ab9fabdb
Red Rose <shyampatel2408@gmail.com>
2018-03-09 19:05:09 +0530

    Add /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var mongoose = require('./db/mongoose');
+var ObjectID=require('mongodb').ObjectID;
 var Todo = require('./models/todo');
 var use = require('./models/user');
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
 
 app.use(bodyParser.json());
 
@@ -26,6 +28,22 @@ app.get('/todos',(req,res)=>{
     },()=>{
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    const id=req.params.id 
+
+        if(!ObjectID.isValid(id)){
+            return res.status(404).send()
+        }
+        Todo.findById(id).then((todo)=>{
+            if(!todo){
+                res.status(404).send();
+            }
+            res.send({todo});
+        });
+},(e)=>{
+   res.status(400).send();
 });
 
 
